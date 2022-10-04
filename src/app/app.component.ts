dd9b6f2c3b1928064ee15c9e2e4288c1b45dbdb6
Akshay Kumbhare <52664007+Akshay7-max@users.noreply.github.com>
2022-10-04 19:43:28 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project2';
+  
+  firstname: string = "Akshay";
+  lastname: string = "Kumbhare";
+  age: number = 27;
+  receivenewsletters: boolean = true;
+  gender: string = "Male";
+  country: string = "India";
+  address: string = "http://www.facebook.com";
+
+  ChangeDate() {
+    this.firstname = "Black";
+    this.lastname = "Adam";
+    this.age = 21;
+    this.receivenewsletters = true;
+    this.gender = "Male";
+    this.country = "India";
+    this.address = "http://www.youtube.com";
+  }
 }
